f8bb4182e0d26c3af9a53dfcf762bef13925f8c1
Nacho <nachot311296@gmail.com>
2022-05-04 21:26:05 -0300

    acomodo funcion especial

--- src/ejemplo/jflex/MiParserSym.java
+++ src/ejemplo/jflex/MiParserSym.java
@@ -11,35 +11,35 @@ public class MiParserSym {
   public static final int DISPLAY = 40;
   public static final int SUMAIMPAR = 24;
   public static final int ENDDECLARE_SECTION = 42;
-  public static final int CORCHETESC = 15;
-  public static final int CORCHETESA = 14;
+  public static final int CORCHETESC = 16;
+  public static final int CORCHETESA = 15;
   public static final int RESTA = 3;
   public static final int INTEGER = 25;
-  public static final int DOS_PUNTOS = 18;
-  public static final int PUNTOCOMA = 17;
-  public static final int PARENTESISC = 13;
-  public static final int PARENTESISA = 12;
+  public static final int DOS_PUNTOS = 19;
+  public static final int PUNTOCOMA = 18;
+  public static final int PARENTESISC = 14;
+  public static final int PARENTESISA = 13;
   public static final int BOOLEAN_LITERAL = 21;
   public static final int IDENTIFICADOR = 20;
   public static final int NOT = 33;
   public static final int AND = 31;
+  public static final int IGUAL = 6;
   public static final int OR = 32;
   public static final int SUMA = 2;
   public static final int IF = 37;
-  public static final int IGUAL_IGUAL = 6;
+  public static final int IGUAL_IGUAL = 7;
   public static final int EOF = 0;
   public static final int BOOLEAN = 27;
   public static final int INPUT_INT = 28;
   public static final int error = 1;
-  public static final int COMA = 16;
+  public static final int COMA = 17;
   public static final int INPUT_BOOL = 30;
-  public static final int MENOR = 10;
-  public static final int DESIGUAL = 7;
-  public static final int ASIGNACION = 19;
-  public static final int MAYOR = 8;
+  public static final int MENOR = 11;
+  public static final int DESIGUAL = 8;
+  public static final int MAYOR = 9;
   public static final int ELSE = 39;
   public static final int WHILE = 34;
-  public static final int MAYOR_IGUAL = 9;
+  public static final int MAYOR_IGUAL = 10;
   public static final int FLOAT = 26;
   public static final int THEN = 38;
   public static final int PROGRAM_SECTION = 43;
@@ -47,7 +47,7 @@ public class MiParserSym {
   public static final int INPUT_FLOAT = 29;
   public static final int STRING = 45;
   public static final int ENDPROGRAM_SECTION = 44;
-  public static final int MENOR_IGUAL = 11;
+  public static final int MENOR_IGUAL = 12;
   public static final int FLOAT_LITERAL = 23;
   public static final int DIVISION = 5;
   public static final int DO = 35;
@@ -61,6 +61,7 @@ public class MiParserSym {
   "RESTA",
   "MULTIPLICACION",
   "DIVISION",
+  "IGUAL",
   "IGUAL_IGUAL",
   "DESIGUAL",
   "MAYOR",
@@ -74,7 +75,6 @@ public class MiParserSym {
   "COMA",
   "PUNTOCOMA",
   "DOS_PUNTOS",
-  "ASIGNACION",
   "IDENTIFICADOR",
   "BOOLEAN_LITERAL",
   "INTEGER_LITERAL",
