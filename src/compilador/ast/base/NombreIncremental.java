f874c23bfb1f3a3de2dbc69bb6fa772956292763
Facundo <facufigliuolo@gmail.com>
2022-05-29 21:28:58 -0300

    increment

---
+++ src/compilador/ast/base/NombreIncremental.java
@@ -0,0 +1,32 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package compilador.ast.base;
+
+/**
+ *
+ * @author facundo
+ */
+public class NombreIncremental {
+    
+    private Integer numero;
+    
+    public NombreIncremental(Integer numero){
+        this.numero = numero;
+    }
+
+    public Integer getNumero() {
+        return numero;
+    }
+
+    public void setNumero(Integer numero) {
+        this.numero = numero;
+    }
+    
+    public Integer increment(){
+        return this.numero += 1;
+    }
+    
+    
+}
